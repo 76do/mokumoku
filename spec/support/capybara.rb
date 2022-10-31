408cc87034433eee4cc784137f1b0fa0277d6c80
76do <isshi0210@gmail.com>
2022-10-31 10:22:20 +0900

    add: specファイルをgit管理下に追加

---
+++ spec/support/capybara.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'selenium-webdriver'
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
+  end
+end
